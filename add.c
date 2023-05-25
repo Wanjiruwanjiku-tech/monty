d6dfb9c11dc727c6d09cb33f526b297a74324a25
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-05-25 02:29:39 -0700

    Monty

--- add.c
+++ add.c
@@ -9,9 +9,9 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack  == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next += (*stack)->n;
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
 }
--- pop.c
+++ pop.c
@@ -12,13 +12,13 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	top = *stack;
-	*stack = (*stack)->next;
+	*stack = top->next;
 
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
+	if (top->next)
+		top->next->prev = NULL;
 	free(top);
 }
